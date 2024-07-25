1e732b623c18793254d4e413822a7fe97ade8ed6
DrDendrite <ctpineda1@gmail.com>
2024-07-24 20:19:21 -0400

    CORS installed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -80,8 +80,8 @@ class Api {
   }
 }
 
-const base_url ="api.dendriteprojectaround.ignorelist.com";
-
+const base_url ="http://api.dendriteprojectaround.ignorelist.com";
+//const base_url ="http://localhost:8001";
 
 export const api = new Api({
   baseUrl: base_url,
